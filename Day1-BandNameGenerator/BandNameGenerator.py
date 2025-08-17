3ef9ce929ac4b17367c553fae2d11ed62ee0ae86
IsaiahLugo <isaiahlugom@gmail.com>
2025-08-17 00:30:21 -0400

    Day1-BandNameGenerator

---
+++ Day1-BandNameGenerator/BandNameGenerator.py
@@ -0,0 +1,12 @@
+#Day 1 Mini-Project Band Name Generator
+
+print('Welcome to the Band Name Generator!')
+name = input('Hello, what is your name?\n')
+
+print(f"Hi {name}!")
+
+city = input('What city are you from?\n')
+pet_name = input('What is the name of your pet?\n')
+
+
+print(f"Your band name could be {city} {pet_name}!")
