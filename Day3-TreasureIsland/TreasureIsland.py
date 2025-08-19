88505b1d88c9810cd470b2326d24c516b08f9026
IsaiahLugo <isaiahlugom@gmail.com>
2025-08-18 23:54:14 -0400

    TreasureIsland.py

--- Day3-TreasureIsland/TreasureIsland.py
+++ Day3-TreasureIsland/TreasureIsland.py
@@ -1,7 +1,26 @@
-print('Welcome to odd or even!')
-number = int(input('Enter a whole number: '))
+print('Welcome to the rollercoaster!')
+height = int(input('How tall are you in cm: '))
+bill = 0
 
-if number % 2 == 0:
-    print('Even!')
+if height >= 120:
+    print('You are tall enough to ride the rollercoaster.')
+    age = int(input('How old are you? '))
+    
+    if age <= 12:
+        print('Your ticket cost 6$.')   
+        bill = 6
+    elif age <=18:
+        print('Your ticket cost 7$')
+        bill = 7
+    else:
+        print('Your ticket cost 12$.')
+        bill = 12
+        
+    wants_photo = input('Do you want a photo? Enter y for yes and n for no: ')
+    if wants_photo == 'y':
+        bill += 3
+        
+    print(f'Your total bill is {bill}.')
+
 else:
-    print('Odd!')
+    print('You are not tall enough to ride the rollercoaster.')
