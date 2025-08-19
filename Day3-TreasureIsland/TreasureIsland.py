8cd74b65b1870f061e854801ea0d607543364a0d
IsaiahLugo <isaiahlugom@gmail.com>
2025-08-18 23:55:39 -0400

    TreasureIsland.py

--- Day3-TreasureIsland/TreasureIsland.py
+++ Day3-TreasureIsland/TreasureIsland.py
@@ -7,13 +7,13 @@ if height >= 120:
     age = int(input('How old are you? '))
     
     if age <= 12:
-        print('Your ticket cost 6$.')   
+        print('Your ticket cost $6.')   
         bill = 6
     elif age <=18:
-        print('Your ticket cost 7$')
+        print('Your ticket cost $7')
         bill = 7
     else:
-        print('Your ticket cost 12$.')
+        print('Your ticket cost $12.')
         bill = 12
         
     wants_photo = input('Do you want a photo? Enter y for yes and n for no: ')
