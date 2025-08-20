20aee163b7ba256f804ba56ee04438879a058e0b
IsaiahLugo <isaiahlugom@gmail.com>
2025-08-19 22:57:55 -0400

    TreasureIsland.py

--- Day3-TreasureIsland/TreasureIsland.py
+++ Day3-TreasureIsland/TreasureIsland.py
@@ -1,26 +1,28 @@
-print('Welcome to the rollercoaster!')
-height = int(input('How tall are you in cm: '))
-bill = 0
+print('Welcome to Treasure Island.')
+print('Your mission is to find the treasure.')
 
-if height >= 120:
-    print('You are tall enough to ride the rollercoaster.')
-    age = int(input('How old are you? '))
+print('You\'re at a crossroads. Which direction do you want to go?')
+direction = input('\tType "left or "right": ')
+
+if direction == 'right':
+    print('You got mauled by a bear...Game Over!')
+elif direction == 'left':
+    print('You\'ve come to a lake. In the middle of the lake there is an island.')
+    print('How will your cross the lake?')
+    crossing_lake = input('\t Type "swim" or "wait": ')
     
-    if age <= 12:
-        print('Your ticket cost $6.')   
-        bill = 6
-    elif age <=18:
-        print('Your ticket cost $7')
-        bill = 7
-    else:
-        print('Your ticket cost $12.')
-        bill = 12
+    if crossing_lake == 'swim':
+        print('You don\'t know how to swim. You sunk to the sea floor...Game Over!')
+    elif crossing_lake == 'wait':
+        print('A boat arrived and you rode the boat to the island.')
+        print('You arrive at the island unharmed. There is a house with three doors.')
+        print('One door is red, another blue, and the last is green. What door will you select?')
+        house_door_color = input('\t Type "red" or "blue" or "green": ')
         
-    wants_photo = input('Do you want a photo? Enter y for yes and n for no: ')
-    if wants_photo == 'y':
-        bill += 3
-        
-    print(f'Your total bill is {bill}.')
-
-else:
-    print('You are not tall enough to ride the rollercoaster.')
+        if house_door_color == 'red':
+            print('You enter a room full of fire...Game Over!')
+        elif house_door_color == 'blue':
+            print('You enter a room full of snakes...Game Over!')
+        elif house_door_color == 'green':
+            print('You enter a room filled with money. YOU HAVE FOUND THE TREASURE!')
+            print('You win!!!')
