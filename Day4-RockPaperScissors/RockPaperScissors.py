c24ed2655aa0654d1b0518ef446786d43263023c
IsaiahLugo <isaiahlugoc@gmail.com>
2025-08-21 12:31:52 -0400

    RockPaperScissors.py

--- Day4-RockPaperScissors/RockPaperScissors.py
+++ Day4-RockPaperScissors/RockPaperScissors.py
@@ -1,12 +1,22 @@
+import random
+import RPS_ART
 
-states = ['CA', 'NY', 'NJ', 'TX']
+choices = [RPS_ART.rock, RPS_ART.paper, RPS_ART.scissors]
 
+users_choice = int(input('What is your choice? Type "0" for rock, "1" for paper, "2" for scissors: '))
+computer_choice = random.randint(0,2)
 
-states[3] = 'PA'
+print(choices[users_choice])
+print("The computer chose:")
+print(choices[computer_choice])
 
-states.append('TX')
-print(states[4])
-print(states)
-
-states.pop(4)
-print(states)
+if users_choice == computer_choice:
+    print('Draw')
+elif users_choice == 0 and computer_choice == 2:
+    print('You Win!')
+elif users_choice == 1 and computer_choice == 2:
+    print('You Lose!')
+elif users_choice == 2 and computer_choice == 0:
+    print('You Lose')
+elif users_choice == 2 and computer_choice == 1:
+    print('You Win!')
