380651b4cb00590321ed40a1d277bebf680a79d6
IsaiahLugo <isaiahlugoc@gmail.com>
2025-09-02 22:50:33 -0400

    Hangman.py

--- Day7-Hangman/Hangman.py
+++ Day7-Hangman/Hangman.py
@@ -6,6 +6,7 @@ game_is_ongoing = True
 hangman_word = random.choice(hangman_word_bank.words)
 displayed_word = []
 total_matches = 0
+lives = 6
 
 print(hangman_word_bank.hangman_logo)
 print('Welcome to hangman!!!')
@@ -18,6 +19,7 @@ print(hangman_word)
 
 while game_is_ongoing == True:
     matches_this_turn = 0
+    print(hangman_word_bank.HANGMANPICS[-lives - 1])
     user_guess = input('Enter a letter: ').lower()
     
     for index, letter in enumerate(hangman_word):
@@ -28,14 +30,21 @@ while game_is_ongoing == True:
        
     if matches_this_turn == 0:
         print(f'I\'m sorry "{user_guess}" is not part of the chosen word...')    
+        lives -= 1
         
     print(''.join(displayed_word))
     
     if total_matches == len(hangman_word):
         print('You win!')
         break
+    
+    if lives == 0:
+        print('You lose!')
+        print
+        break
+    
 
 
 
-#-2 If the user guesses correctly replace the corresponding space with the letter if not -1 life
+
 #-3The user has 6 lives if lives reaches 0 they lose --If all empty spaces are filled the player wins
