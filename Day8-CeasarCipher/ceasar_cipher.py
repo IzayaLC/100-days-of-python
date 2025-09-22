64ddcf965c6af6e9a43fe03d03e33c090c3c898c
IsaiahLugo <isaiahlugoc@gmail.com>
2025-09-21 23:35:58 -0400

    ceasar_cipher.py

--- Day8-CeasarCipher/ceasar_cipher.py
+++ Day8-CeasarCipher/ceasar_cipher.py
@@ -2,16 +2,31 @@
 #-create a encrypt and decrypt function
 
 def encryption(plaintext, key):
+    encrypted_characters = []
     for character in plaintext:
+        
         character_index = alphabet.index(character)
-        encrypted_index = character_index + key
+        encrypted_index = (character_index + key) % 26
         
         cipher_character = alphabet[encrypted_index]
+        encrypted_characters.append(cipher_character)
         
-        print(f"Plaintext character: {character} ")
-        print(f"Ciphertext character: {cipher_character}")
+    ciphertext = "".join(encrypted_characters)
+    return ciphertext
 
-#The user will enter the key (shift number)
+def decryption(ciphertext, key):
+    decrypted_characters = []
+    for character in ciphertext:
+        
+        encrypted_index = alphabet.index(character)
+        decrypted_index = (encrypted_index - key) % 26
+        
+        plaintext_char = alphabet[decrypted_index] 
+        decrypted_characters.append(plaintext_char)
+        
+    plaintext = "".join(decrypted_characters)
+    return plaintext
+
 #THe user will enter the word they wish to encrypt or decrypt
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m",
@@ -21,4 +36,4 @@ print("Welcome to the Ceasar Cipher!")
 decision = input("Type 'e' to encrypt, type 'd' to decrypt: ")
 
 if decision == "e":
-    encryption('a', 3)
+    print(encryption("day", 3))
