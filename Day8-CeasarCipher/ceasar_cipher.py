356ca5841b706a4da954482ad67a12b9519e0281
IsaiahLugo <isaiahlugoc@gmail.com>
2025-11-06 15:21:13 -0500

    README.md

--- Day8-CeasarCipher/ceasar_cipher.py
+++ Day8-CeasarCipher/ceasar_cipher.py
@@ -54,19 +54,23 @@ def decryption(ciphertext, key=3):
     plaintext = "".join(decrypted_characters)
     return plaintext
 
-
-print(r"""
-  _____                              _____  _       _               
- / ____|                            / ____|(_)     (_)              
-| |     __ _  ___  ___  __ _ _ __  | |     _ _ __   _ ___ ___  ___ 
-| |    / _` |/ _ \/ __|/ _` | '__| | |    | | '_ \ | / __/ __|/ _ \
-| |___| (_| |  __/\__ \ (_| | |    | |____| | |_) || \__ \__ \  __/
- \_____\__,_|\___||___/\__,_|_|     \_____|_| .__/ |_ |___/___/\___|
-                                            | |   _/ |              
-                                            |_|  |__/               
-""")
-
-
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
 print("Welcome to the Ceasar Cipher!")
 decision = input("Type 'e' to encrypt, type 'd' to decrypt: ")
 
@@ -82,3 +86,5 @@ elif decision == "d":
     plaintext = decryption(text, shift_number)
     
     print(f"Plaintext generated:\"{plaintext}\"")
+    
+    
